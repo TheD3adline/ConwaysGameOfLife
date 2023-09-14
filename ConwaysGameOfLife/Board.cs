bc119f1aeafc7d5ca95807f9c4bb33756665862f
Michael Fessler <65716100+TheD3adline@users.noreply.github.com>
2023-09-14 11:19:16 +0200

    Add project files.

---
+++ ConwaysGameOfLife/Board.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife
+{
+    internal class Board
+    {
+        const char CHARACTER_ALIVE = '#';
+        const char CHARACTER_DEADL = '.';
+
+        public Cell[,] Fields { get; private set; }
+        public int Generation { get; private set; }
+
+        public Board(int width, int height) 
+        {
+            Fields = new Cell[width, height];
+            Generation = 1;
+
+            for (int y = 0; y < Fields.GetLength(1); y++)
+            {
+                for (int x = 0; x < Fields.GetLength(0); x++)
+                {
+                    Fields[x, y] = new Cell(false);
+                }
+            }
+        }
+
+        public void Run()
+        {
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                //Draw();
+                //Update();
+                Generation += 1;
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ ConwaysGameOfLife/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife
+{
+    internal class Cell
+    {
+        public bool Alive { get; set; }
+        public bool AliveNextRound { get; set; }
+        public bool LastGenerationStatus { get; set; }
+        public int AmountOfNeighbours { get; set; }
+
+        public Cell(bool alive) 
+        {
+            Alive = alive;
+        }
+    }
+}
